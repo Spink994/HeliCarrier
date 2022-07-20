6f0caba67d2095405b459d20be89f03b228b0c1c
Hespeehaaru <slowpacerapper@gmail.com>
2022-07-20 10:50:26 +0100

    PWA Commit
    Pwa Commit

---
+++ public/sw_spink.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "version-2";
+
+//Install
+self.addEventListener("install", (e) =>
+  console.log("Service Worker Installed!")
+);
+
+//Activate
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker Activated!");
+
+  //Removing redundant cache versions
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log("Cleared Old Caches");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+//Fetch
+self.addEventListener("fetch", (e) => {
+  console.log("Fetching data");
+
+  e.respondWith(
+    fetch(e.request)
+      .then((resposnse) => {
+        //Making a clone of the response
+        const clonedResponse = resposnse.clone();
+
+        //Opening the cache
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, clonedResponse);
+        });
+
+        return resposnse;
+      })
+      .catch(() => caches.match(e.request).then((response) => response))
+  );
+});
+
