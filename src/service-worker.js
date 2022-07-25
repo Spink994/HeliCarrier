525723fe0105a59860d9da58524caa8160403529
Hespeehaaru <slowpacerapper@gmail.com>
2022-07-25 06:10:59 +0100

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -70,13 +70,19 @@ self.addEventListener("message", (event) => {
   }
 });
 
-// Any other custom service worker logic can go here.
-// Set up asset cache
-const apiPath = "api.graphql.jobs";
+
+// An example runtime caching route for requests that aren't handled by the
+// precache, in this case same-origin .png requests like those from in public/
 registerRoute(
-  ({ request }) => apiPath.match(request.destination),
+  // Add in any other file extensions or routing criteria as needed.
+  ({ url }) =>
+    url.origin === self.location.origin && url.pathname.endsWith(".jobs"), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: "apiData",
-    plugins: [{ statuses: [0, 200] }],
+    plugins: [
+      // Ensure that once this runtime cache reaches a maximum size the
+      // least-recently used images are removed.
+      new ExpirationPlugin({ maxEntries: 50 }),
+    ],
   })
 );
